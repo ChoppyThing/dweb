e8979989215a9492236c1d2924f6a46ee744544e
Jerry C <jerry.comerasamy@homunity.fr>
2019-05-12 22:24:59 +0200

    Added homepage

--- source/home/homepage.d
+++ source/home/homepage.d
@@ -1,14 +1,27 @@
 module home.homepage;
 
 import vibe.vibe;
+import database.database;
 
 @path("/")
 class Home
 {
+	DatabaseConnection database;
+
+	this()
+	{
+		if (!this.database) {
+			this.database = new DatabaseConnection();
+		}
+	}
+
 	@method(HTTPMethod.GET)
 	@path("/")
-	void homepage()
+	void homepage(int page = 1)
 	{
-		render!"home/homepage.dt";
+		auto posts = this.database.getPosts(page);
+		auto postCount = this.database.getPostCount();
+
+		render!("home/homepage.dt", posts, postCount, page);
 	}
 }
